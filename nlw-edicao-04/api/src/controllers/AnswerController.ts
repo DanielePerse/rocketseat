58368db06c1d71f534693067fe5b3d18898a6c01
danieleperse <danieleperse3@gmail.com>
2021-02-26 11:09:29 -0300

    validaçoes efetudadas - nota do usuario sendo salva com sucesso

---
+++ nlw-edicao-04/api/src/controllers/AnswerController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+
+class AnswerController {
+
+      // http://localhost:3333/answers/10?u=6207a574-1556-415e-96e9-1cd7968349ef
+    // Router params -> obrigado a passar
+    // routes.get(/answers/:values) -> esse value é o que precisaremos buscar
+    // se tivesse mais um parametro, bastaria colocar : e identifica-lo
+    // /answers/10/5/4 -> :values/:nota/:batata (exemplo)
+    // query params -> não são obrigatorios. usado em busca, paginação (chave=valor)
+
+  async execute(request: Request, response: Response) {
+    const { value } = request.params;
+    const { u } = request.query;
+
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+    const surveyUser = await surveysUsersRepository.findOne({
+      id: String(u)
+    });
+
+    if (!surveyUser) {
+      return response.status(400).json({
+        error: "Survey User does not exists!"
+      });
+    };
+
+    surveyUser.value = Number(value);
+
+    await surveysUsersRepository.save(surveyUser);
+
+    return response.json(surveyUser);
+  };
+};
+
+export { AnswerController }
